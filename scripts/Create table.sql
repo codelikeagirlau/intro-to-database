a7eee690f39a2c47d78102ac3b40c7c29571cfaa
N <xy@Natashas-MBP.gateway>
2018-08-04 08:18:27 +1000

    Added example tables

--- scripts/Create table.sql
+++ scripts/Create table.sql
@@ -5,7 +5,13 @@ CREATE TABLE IF NOT EXISTS `User` (
     `LastName` VARCHAR(50) NOT NULL,
     `Email` VARCHAR(50),
     `DateOfBirth` DATE
-)
+);
+
+CREATE TABLE IF NOT EXISTS `Title` (
+    `TitleID` INT(64) AUTO_INCREMENT PRIMARY KEY,
+    `Title` VARCHAR(50) NOT NULL
+);
+
 
 -- Populate new table
 INSERT INTO User
@@ -14,8 +20,16 @@ VALUES      ('Elizabeth',    'Windsor',      'queen@palace.gov.uk',  '1926-04-21
 
 INSERT INTO User
             (FirstName,     LastName,       Email,                  DateOfBirth)
-VALUES      ('William',    'Wales',         'william@wales.com',    '1982-06-21')
+VALUES      ('Charles',     'Windsor',      'charles@wales.com',    '1948-11-14');
 
 INSERT INTO User
             (FirstName,     LastName,       Email,                  DateOfBirth)
-VALUES      ('Henry',       'Wales',      'harry@wales.com',        '1984-09-15')
+VALUES      ('William',    'Wales',         'william@wales.com',    '1982-06-21');
+
+INSERT INTO User
+            (FirstName,     LastName,       Email,                  DateOfBirth)
+VALUES      ('Henry',       'Wales',      'harry@wales.com',        '1984-09-15');
+
+INSERT INTO User
+            (FirstName,     LastName,       Email,                  DateOfBirth)
+VALUES      ('Catherine',   'Middleton',    'kate@wales.com',       '1982-01-09');
